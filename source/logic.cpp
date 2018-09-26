0d666b297abd8f551652ff3227e58a07944609dc
Adam Herczig <herczig.adam@gmail.com>
2018-09-26 15:33:21 +0200

    Update logic.cpp

--- source/logic.cpp
+++ source/logic.cpp
@@ -109,3 +109,100 @@ vector<int> retVect(vector<int> &vec)
     vec.resize(100,33);
     return vec;
 }
+
+
+tricky_series::tricky_series()
+{
+    n_element_of_palindromes=1;
+
+}
+
+tricky_series::tricky_series(int n)
+{
+    this->n_element_of_palindromes=n;
+}
+
+void tricky_series::argGiven(int n)
+{
+    n_element_of_palindromes=n;
+
+
+}
+
+void tricky_series::isPalindrome()
+{
+    if(n_element_of_palindromes>MAXN || !n_element_of_palindromes)
+        throw "palindrome error";
+    int i=0;
+    cout<<"now type the string(s) which(es) have to check to palindrome or not!"<<endl;
+    palindromes.resize(n_element_of_palindromes);
+    for(; i<n_element_of_palindromes; i++)
+        getline(cin,palindromes[i]);
+    i=0;
+    for(;i<n_element_of_palindromes;i++)
+    {
+        if(palindromes[i]==string(palindromes[i].rbegin(),palindromes[i].rend()))
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    for(string x:palindromes)
+        cout<<x<<endl;
+
+}
+
+void tricky_series::maxvalueOfTwoNumbers()
+{
+    int i,j;
+    i=j=0;
+    cin>>i;
+    if(i<1 || i>100)
+        throw "Testcase number error";
+
+        stringToInt(i);
+        cout<<"stringToINT is called"<<endl;
+
+
+    j=0;
+    while(j<i+1)
+    {
+        cout<<ab[j]<<" ";
+        j++;
+        cout<<ab[j]<<" ";
+        j++;
+    }
+
+}
+
+
+void tricky_series::convertListToVector()
+{
+
+    list<string> listpatterns;
+
+    listpatterns.push_back("first");
+    listpatterns.push_back("second");
+    listpatterns.push_back("third");
+    listpatterns.push_back("fourth");
+    palindromes.assign(listpatterns.begin(),listpatterns.end());
+
+    for(string x : palindromes)
+        cout<<x<<endl;
+
+}
+
+void tricky_series::stringToInt(int i)
+{
+
+    cout<<"fukk"<<endl;
+    while(i)
+    {
+        getline(cin,s);
+        istringstream is(s);
+        ab.assign((istream_iterator<int>(is)),istream_iterator<int>());
+        i--;
+
+    }
+
+
+}
